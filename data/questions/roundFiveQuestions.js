692ece86ab7cf7dafa39318adbed47a6afb5706e
Harsh-Kashyap-github <harsh2003.hk@gmail.com>
2024-12-21 14:12:46 +0530

    photo link update

--- data/questions/roundFiveQuestions.js
+++ data/questions/roundFiveQuestions.js
@@ -2,26 +2,26 @@ export const RoundFiveQuestions = [
   {
     id: 'X8FJ9D3A',
     photoPath:`https://github.com/Harsh-Kashyap-github/Nature-Nexus/blob/main/public/photos/photo1.jpeg?raw=true`, 
-    wordLengths: [7,5], 
+    wordLengths: [4,2], 
   },
   {
     id: 'L3KQ7T1P',
     photoPath:`https://github.com/Harsh-Kashyap-github/Nature-Nexus/blob/main/public/photos/photo2.jpeg?raw=true`, 
-    wordLengths: [4,2], 
+    wordLengths: [6],
   },
   {
     id: 'N6YV2C4R',
     photoPath:`https://github.com/Harsh-Kashyap-github/Nature-Nexus/blob/main/public/photos/photo3.jpeg?raw=true`, 
-    wordLengths: [4,9],
+    wordLengths: [4,5],
   },
   {
     id: 'Q1ZK5L9M',
     photoPath:`https://github.com/Harsh-Kashyap-github/Nature-Nexus/blob/main/public/photos/photo4.jpeg?raw=true`, 
-    wordLengths: [4, 5], 
+    wordLengths: [9], 
   },
-  // {
-  //   id: 'T5XW3N8K',
-  //   photoPath: '../../data/photos/photo5.jpeg', // Replace with your own image URL
-  //   wordLengths: [5, 7, 3], // Exact lengths of each word in the answer
-  // },
+  {
+    id: 'T5XW3N8K',
+    photoPath: 'https://github.com/Harsh-Kashyap-github/Nature-Nexus/blob/main/public/photos/photo5.jpeg?raw=true', 
+    wordLengths: [4,9],
+  },
 ];
